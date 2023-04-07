cccf9b21a4494918b6bc0af87d4c312cce59842a
Stephen Olaiya <54467284+hesblac@users.noreply.github.com>
2023-04-07 15:37:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "instance_sg" {
 
 # Launch two Ubuntu EC2 instances
 resource "aws_instance" "lafiaji_instance" {
-  ami = "ami-0144c4a527d2e6da0"
+  ami = "ami-0e16c3bc75f23e32b"
   instance_type = "t2.micro"
   count = 2
   key_name = "mykey"
