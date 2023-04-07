538d7dac7e8d1a9f393a05ccc64a8ffea9b48f96
Stephen Olaiya <54467284+hesblac@users.noreply.github.com>
2023-04-07 16:14:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # Create an S3 bucket
 resource "aws_s3_bucket" "somffin_bucket" {
   bucket = "frontier667-bucket"
-
+}
 
 # Launch two Ubuntu EC2 instances
 resource "aws_instance" "lafiaji_instance" {
